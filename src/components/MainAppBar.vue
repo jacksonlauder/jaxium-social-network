93fcbf6db8bb3704c400178959fadeb88e57e003
Jackson Lauder <jacksonelauder@gmail.com>
2021-02-24 14:56:47 -0700

    debugging

--- src/components/MainAppBar.vue
+++ src/components/MainAppBar.vue
@@ -1,21 +1,21 @@
 <template>
-  <v-card>
-    <v-app-bar app flat absolute tile>
-      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+    <v-app-bar color="blue-grey lighten-3" app flat absolute tile>
+      <!-- <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon> -->
       <!-- <v-spacer></v-spacer> -->
-        <v-img
-          alt="Jaxium Logo"
-          class="shrink mr-2"
-          contain
-          src="../assets/jaxium-social-network_200.png"
-          width="40"
-        />
+        <v-avatar tile>
+          <v-img
+            alt="Jaxium Logo"
+            class="shrink mr-2"
+            contain
+            src="../assets/jaxium-social-network_200.png"
+            max-height="40"
+            max-width="40"
+          ></v-img>
+        </v-avatar>
       <v-toolbar-title>Jaxium</v-toolbar-title>
       <!-- <v-spacer></v-spacer> -->
     </v-app-bar>
-    <v-navigation-drawer v-model="drawer" app>
-    </v-navigation-drawer>
-  </v-card>
+    <!-- <v-navigation-drawer v-model="drawer" app></v-navigation-drawer> -->
 </template>
 
 <script>
