d52ffa9e413450ff69a0b6c869e134268fd9fead
Jackson Lauder <jacksonelauder@gmail.com>
2021-02-23 17:51:53 -0700

    Added stuff

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,27 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <v-card width="700" class="mx-auto mt-5 pa-2" flat>
+    <v-card-title class="justify-center pb-3">
+      <h1>Welcome to Pub Quiz!</h1>
+    </v-card-title>
+    <v-divider></v-divider>
+    <v-card-text>With the Coronavirus going on, meeting up at the pub might be a little difficult.
+      Continue the fun with this virtual pub quiz! Invite your friends and family for trivia fun.
+      <br>Playing is really simple. One person hosts the game and judges questions,
+        the rest join the game by entering a code and answering questions.
+      <br>To play, a user must have registered for an account. You can click Register or Login to get started.</v-card-text>
+    <v-card-actions class="pt-4">
+      <v-btn rounded depressed large color="primary" to="/register">Register</v-btn>
+      <v-spacer></v-spacer>
+      <v-btn rounded depressed large color="success" to="/login">Login</v-btn>
+      <!-- <v-spacer></v-spacer>
+      <v-btn rounded depressed large color="success" to="/hostoruser">Play</v-btn> -->
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+  name: 'Home'
 }
 </script>
