ce72721060df1f1607e5ede5f33d9cd99e2080b6
Jackson Lauder <jacksonelauder@gmail.com>
2021-02-23 22:02:51 -0700

    Added styling

--- src/components/MainAppBar.vue
+++ src/components/MainAppBar.vue
@@ -1,17 +1,21 @@
 <template>
-<div>
-  <v-navigation-drawer
-    v-model="drawer"
-    app
-  >
-    <!--  -->
-  </v-navigation-drawer>
-
-  <v-app-bar app flat absolute>
-    <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
-    <v-toolbar-title>Jaxium</v-toolbar-title>
-  </v-app-bar>
-</div>
+  <v-card>
+    <v-app-bar app flat absolute tile>
+      <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon>
+      <!-- <v-spacer></v-spacer> -->
+        <v-img
+          alt="Jaxium Logo"
+          class="shrink mr-2"
+          contain
+          src="../assets/jaxium-social-network_200.png"
+          width="30"
+        />
+      <v-toolbar-title>Jaxium</v-toolbar-title>
+      <!-- <v-spacer></v-spacer> -->
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" app>
+    </v-navigation-drawer>
+  </v-card>
 </template>
 
 <script>
