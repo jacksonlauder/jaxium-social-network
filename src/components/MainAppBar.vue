ad20aab10b766c96f6696afe68eb28bf31e9dd59
Jackson Lauder <jacksonelauder@gmail.com>
2021-02-24 15:13:23 -0700

    bug fixing

--- src/components/MainAppBar.vue
+++ src/components/MainAppBar.vue
@@ -1,25 +1,18 @@
 <template>
-    <v-app-bar color="blue-grey lighten-3" app flat absolute tile>
-      <!-- <v-app-bar-nav-icon @click="drawer = !drawer"></v-app-bar-nav-icon> -->
-      <!-- <v-spacer></v-spacer> -->
-        <v-avatar tile>
-          <v-img
-            alt="Jaxium Logo"
-            class="shrink mr-2"
-            contain
-            src="../assets/jaxium-social-network_200.png"
-            max-height="40"
-            max-width="40"
-          ></v-img>
-        </v-avatar>
-      <v-toolbar-title>Jaxium</v-toolbar-title>
-      <!-- <v-spacer></v-spacer> -->
-    </v-app-bar>
-    <!-- <v-navigation-drawer v-model="drawer" app></v-navigation-drawer> -->
+  <v-app-bar color="blue-grey lighten-3" app flat absolute tile>
+    <v-img
+      class="mx-2"
+      src="../assets/jaxium-social-network_200.png"
+      max-height="40"
+      max-width="40"
+      contain
+    ></v-img>
+    <v-toolbar-title>Jaxium</v-toolbar-title>
+  </v-app-bar>
 </template>
 
 <script>
-  export default {
-    data: () => ({ drawer: null }),
-  }
+export default {
+  data: () => ({ drawer: null }),
+};
 </script>
