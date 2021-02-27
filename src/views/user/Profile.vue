f35fe395d3d481132e83312e071439ecf7c2263b
Jackson Lauder <jacksonelauder@gmail.com>
2021-02-26 17:26:09 -0700

    Stuff

--- src/views/user/Profile.vue
+++ src/views/user/Profile.vue
@@ -1,3 +1,16 @@
 <template>
-  <h1>Profile Route</h1>
+  <v-container fluid>
+    <v-btn
+      elevation="2"
+      fab
+      top
+      left
+      icon
+    >
+    <v-icon>
+      mdi-arrow-left
+    </v-icon>
+    </v-btn>
+    <h1>Profile Route</h1>
+  </v-container>
 </template>
