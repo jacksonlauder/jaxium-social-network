f920f5d2f19826ccff7e92474f44eeb869cc095d
Jackson Lauder <jacksonelauder@gmail.com>
2021-03-17 18:00:37 -0600

    Added message

--- src/components/Message.vue
+++ src/components/Message.vue
@@ -1,11 +1,82 @@
 <template>
   <v-card
-    class="mx-auto mb-5"
+    flat
+    tile
+    class="mx-auto"
     max-width="500"
-    elevation="4"
   >
-    <v-row>
-      <v-avatar />
-    </v-row>
+    <v-list two-line>
+      <template v-for="(item, index) in items">
+        <v-divider
+          v-if="item.divider"
+          :key="index"
+          :inset="item.inset"
+        />
+        <v-list-item
+          v-else
+          :key="item.title"
+        >
+          <v-list-item-avatar>
+            <v-img :src="item.avatar" />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title v-html="item.title" />
+            <v-list-item-subtitle v-html="item.subtitle" />
+          </v-list-item-content>
+
+          <v-btn
+            fab
+            top
+            left
+            icon
+            color="blue-grey darken-3"
+            to=""
+          >
+            <v-icon large>
+              mdi-chevron-right
+            </v-icon>
+          </v-btn>
+        </v-list-item>
+      </template>
+    </v-list>
   </v-card>
 </template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+        title: 'Account Name',
+        subtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, incidunt!'
+      },
+      { divider: true, inset: true },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+        title: 'Account Name',
+        subtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, incidunt!'
+      },
+      { divider: true, inset: true },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+        title: 'Account Name',
+        subtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, incidunt!'
+      },
+      { divider: true, inset: true },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+        title: 'Account Name',
+        subtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, incidunt!'
+      },
+            { divider: true, inset: true },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+        title: 'Account Name',
+        subtitle: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, incidunt!'
+      },
+    ]
+  })
+}
+</script>
--- src/views/SiteHome.vue
+++ src/views/SiteHome.vue
@@ -53,7 +53,7 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
   .theme--light.v-card {
     background: rgba( 255, 255, 255, 0.25 );
     
