882180229056ef5aecf8b6292138f813a035c838
Jackson Lauder <jacksonelauder@gmail.com>
2021-03-17 23:18:37 -0600

    style

--- src/components/NewPostBtn.vue
+++ src/components/NewPostBtn.vue
@@ -23,7 +23,7 @@
 <style scoped>
   .v-btn--is-elevated.v-btn--fab {
     /* box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%), 0 1px 18px 0 rgb(0 0 0 / 12%) !important; */
-    box-shadow: 0px 0px 0px 9px #eceff1 !important;
+    box-shadow: 0px 0px 0px 7px #eceff1 !important;
   }
   .v-btn--fab.v-size--default.v-btn--absolute.v-btn--top {
     top: -20px !important;
