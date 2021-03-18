0c12abf09e310cb52efe3b6287c33dcaafc188f0
Jackson Lauder <jacksonelauder@gmail.com>
2021-03-17 18:36:05 -0600

    Style added notification component

---
+++ src/components/Notification.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-card
+    flat
+    tile
+    class="mx-auto"
+    max-width="500"
+  >
+    <v-list two-line>
+      <template v-for="(item, index) in items">
+        <v-divider
+          v-if="item.divider"
+          :key="index"
+          :inset="item.inset"
+        />
+        <v-list-item
+          v-else
+          :key="item.title"
+        >
+          <v-list-item-avatar>
+            <v-img :src="item.avatar" />
+          </v-list-item-avatar>
+
+          <v-list-item-content>
+            <v-list-item-title v-html="item.title" />
+            <v-list-item-subtitle v-html="item.subtitle" />
+          </v-list-item-content>
+
+          <v-btn
+            fab
+            top
+            left
+            icon
+            color="blue-grey darken-3"
+            to=""
+          >
+            <v-icon large>
+              mdi-dots-horizontal
+            </v-icon>
+          </v-btn>
+        </v-list-item>
+      </template>
+    </v-list>
+  </v-card>
+</template>
+
+<script>
+export default {
+  data: () => ({
+    items: [
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/1.jpg',
+        title: 'Account Name',
+        subtitle: 'Accepted follow request.'
+      },
+      { divider: true, inset: true },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/2.jpg',
+        title: 'Account Name',
+        subtitle: 'Asked to follow you.'
+      },
+      { divider: true, inset: true },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/3.jpg',
+        title: 'Account Name',
+        subtitle: 'Commented on your post.'
+      },
+      { divider: true, inset: true },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/4.jpg',
+        title: 'Account Name',
+        subtitle: 'Liked your post.'
+      },
+            { divider: true, inset: true },
+      {
+        avatar: 'https://cdn.vuetifyjs.com/images/lists/5.jpg',
+        title: 'Account Name',
+        subtitle: 'Accepted follow request.'
+      },
+    ]
+  })
+}
+</script>
--- src/views/user/Notifications.vue
+++ src/views/user/Notifications.vue
@@ -1,6 +1,7 @@
 <template>
   <v-container fluid>
     <v-btn
+      absolute
       fab
       top
       left
@@ -12,9 +13,39 @@
         mdi-arrow-left-bold
       </v-icon>
     </v-btn>
-    <h1>Notifications Route</h1>
+    <v-btn
+      absolute
+      fab
+      top
+      right
+      icon
+      to=""
+      color="blue-grey darken-3"
+    >
+      <v-icon large>
+        mdi-close-circle-outline
+      </v-icon>
+    </v-btn>
+    <v-row class="mt-5">
+      <h1 class="mx-auto mb-10">
+        Notifications
+      </h1>
+    </v-row>
+    <Notification />
   </v-container>
 </template>
+
+<script>
+import Notification from '../../components/Notification'
+
+export default {
+  name: 'Notifications',
+  components: {
+    Notification
+
+  }
+}
+</script>
 
 <style scoped>
 .v-btn--fab.v-size--default.v-btn--absolute.v-btn--top {
