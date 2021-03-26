cd7fcae957f61c1391326a254782d2cbba3952ae
Jackson Lauder <jacksonelauder@gmail.com>
2021-03-25 18:43:33 -0600

    Alot

---
+++ src/components/NewPost-Dialog.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container fluid>
+    <v-row>
+      <v-dialog
+        v-model="show"
+        transition="dialog-bottom-transition"
+        scrollable
+        fullscreen
+      >
+        <v-card height="800px">
+          <v-toolbar
+            flat
+            color="blue-grey lighten-5"
+            class="pt-3"
+          >
+              <v-text-field
+                solo
+                label="Search"
+                rounded
+                prepend-inner-icon="mdi-magnify"
+                hide-details="auto"
+              />
+              <v-btn
+                icon
+                @click="show = false"
+              >
+                <v-icon>mdi-close</v-icon>
+              </v-btn>
+          </v-toolbar>
+        </v-card>
+      </v-dialog>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    props: {
+      value: Boolean
+    },
+    computed: {
+      show: {
+        get () {
+          return this.value
+        },
+        set (value) {
+          this.$emit('input', value)
+        }
+      }
+    }
+  }
+</script>
--- src/views/user/Home.vue
+++ src/views/user/Home.vue
@@ -4,6 +4,7 @@
     class="mt-6"
   >
     <SearchDialog v-model="showSearchDialog" />
+    <NewPostDialog v-model="showNewPostDialog" />
 
     <Post />
     <PostSkeleton />
@@ -37,7 +38,26 @@
         </v-icon>
       </v-btn>
 
-      <NewPostBtn />
+      <v-btn
+        style="border-radius: 50%"
+        fab
+        min-width="56px"
+        width="56px"
+        min-height="56px"
+        height="56px"
+        color="amber darken-3"
+        dark
+        absolute
+        top
+        @click.stop="showNewPostDialog=true"
+      >
+        <v-icon
+          size="35px"
+          color="white"
+        >
+          mdi-plus
+        </v-icon>
+      </v-btn>
 
       <v-btn to="/:username/messages">
         <span class="hidden-sm-and-down">Messages</span>
@@ -75,27 +95,34 @@
 </template>
 
 <script>
-import NewPostBtn from '../../components/NewPostBtn'
 import Post from '../../components/Post'
 import PostSkeleton from '../../components/PostSkeletonLoader'
 import SearchDialog from '../../components/Search-Dialog'
+import NewPostDialog from '../../components/NewPost-Dialog'
 
   export default {
     name: 'Home',
     components: {
-      NewPostBtn,
       Post,
       PostSkeleton,
       SearchDialog,
+      NewPostDialog,
     },
     data () {
       return {
-        showSearchDialog: false
+        showSearchDialog: false,
+        showNewPostDialog: false
       }
     }
   }
 </script>
 
-<style>
-
+<style scoped>
+  .v-btn--is-elevated.v-btn--fab {
+    /* box-shadow: 0 3px 5px -1px rgb(0 0 0 / 20%), 0 6px 10px 0 rgb(0 0 0 / 14%), 0 1px 18px 0 rgb(0 0 0 / 12%) !important; */
+    box-shadow: 0px 0px 0px 7px #eceff1 !important;
+  }
+  .v-btn--fab.v-size--default.v-btn--absolute.v-btn--top {
+    top: -20px !important;
+  }
 </style>
