172118f431f4ba96def5160dc8ff99195e286714
Jackson Lauder <jacksonelauder@gmail.com>
2021-03-31 16:42:49 -0600

    main app bar routing

--- src/components/MainAppBar.vue
+++ src/components/MainAppBar.vue
@@ -28,38 +28,111 @@
       app
     >
       <v-list nav>
-        <v-list-item to="/">
+        <v-list-item
+          v-if="!$store.state.isLoggedIn"
+          to="/"
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title>
               Site Home
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item to="/register">
+
+        <v-list-item
+          v-if="!$store.state.isLoggedIn"
+          to="/register"
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-home</v-icon>
+          </v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title class="mt-1">
               Register
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item to="/login">
+
+        <v-list-item
+          v-if="!$store.state.isLoggedIn"
+          to="/login"
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-login</v-icon>
+          </v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title class="mt-1">
               Login
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
-        <v-list-item to="/:username/home">
+
+        <v-list-item
+          v-if="$store.state.isLoggedIn"
+          @click.prevent="toHome"
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-newspaper-variant-multiple-outline</v-icon>
+          </v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title class="mt-1">
               Home
             </v-list-item-title>
           </v-list-item-content>
         </v-list-item>
+
+        <v-list-item
+          v-if="$store.state.isLoggedIn"
+          @click.prevent="toProfile"
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-account</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="mt-1">
+              Profile
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item
+          v-if="$store.state.isLoggedIn"
+          @click.prevent="toMessages"
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-message</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="mt-1">
+              Messages
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
+        <v-list-item
+          v-if="$store.state.isLoggedIn"
+          @click.prevent="toNotifications"
+        >
+          <v-list-item-icon>
+            <v-icon>mdi-bell</v-icon>
+          </v-list-item-icon>
+          <v-list-item-content>
+            <v-list-item-title class="mt-1">
+              Notifications
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+
         <v-list-item
           v-if="$store.state.isLoggedIn"
           @click.prevent="logout()"
         >
+          <v-list-item-icon>
+            <v-icon>mdi-logout</v-icon>
+          </v-list-item-icon>
           <v-list-item-content>
             <v-list-item-title class="mt-1">
               Logout
@@ -77,10 +150,22 @@ import * as auth from '../services/AuthService'
 export default {
   data: () => ({ drawer: false }),
     methods: {
-    logout: function () {
-      auth.logout()
-      this.$router.push({ name: 'SiteHome' })
+      logout: function () {
+        auth.logout()
+        this.$router.push({ name: 'SiteHome' })
+      },
+      toHome: function () {
+        this.$router.push({ name: 'UserHome', params: { username: this.$store.state.username }})
+      },
+      toProfile: function () {
+        this.$router.push({ name: 'profile', params: { username: this.$store.state.username }})
+      },
+      toMessages: function () {
+        this.$router.push({ name: 'messages', params: { username: this.$store.state.username }})
+      },
+      toNotifications: function () {
+        this.$router.push({ name: 'notifications', params: { username: this.$store.state.username }})
+      }
     }
-  }
-};
+  };
 </script>
