e43d885016bfb602a846ecdf14db444fd3abde3a
Jackson Lauder <jacksonelauder@gmail.com>
2021-03-31 00:01:08 -0600

    express

--- dev-server/index.js
+++ dev-server/index.js
@@ -1,10 +1,24 @@
+/* eslint-disable no-path-concat */
+// eslint-disable-next-line no-unused-vars
+import _ from 'lodash'
 import express from 'express'
 var app = express()
 
-app.get('/', function (req, res) {
-  res.send('hello world')
+import { registerRoutes } from './routes'
+import { setEnvironment } from './config/env'
+
+registerRoutes(app)
+
+app.get('/', (req, res) => {
+  if (process.env.NODE_ENV !== 'production') {
+    return res.send('Running server in dev mode')
+  } else {
+    return res.sendFile('index.html', { root: __dirname + '/../dist/' })
+  }
 })
 
+setEnvironment(app)
+
 app.listen(3000, () => {
-  console.log('Jaxium Social Network Application listening on port 3000!')
+  console.log('Jaxium Social Network Application listening on port 3000 in ' + process.env.NODE_ENV)
 })
--- prod-server/index.js
+++ prod-server/index.js
@@ -1,17 +1,35 @@
 'use strict';
 
+var _lodash = require('lodash');
+
+var _lodash2 = _interopRequireDefault(_lodash);
+
 var _express = require('express');
 
 var _express2 = _interopRequireDefault(_express);
 
+var _routes = require('./routes');
+
+var _env = require('./config/env');
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
+/* eslint-disable no-path-concat */
+// eslint-disable-next-line no-unused-vars
 var app = (0, _express2.default)();
 
+(0, _routes.registerRoutes)(app);
+
 app.get('/', function (req, res) {
-  res.send('hello world');
+  if (process.env.NODE_ENV !== 'production') {
+    return res.send('Running server in dev mode');
+  } else {
+    return res.sendFile('index.html', { root: __dirname + '/../dist/' });
+  }
 });
 
+(0, _env.setEnvironment)(app);
+
 app.listen(3000, function () {
-  console.log('Jaxium Social Network Application listening on port 3000!');
+  console.log('Jaxium Social Network Application listening on port 3000 in ' + process.env.NODE_ENV);
 });
