03dcb0bd7468593e0e2504cac889989409f8b10a
Jackson Lauder <jacksonelauder@gmail.com>
2021-04-01 17:18:29 -0600

    styling

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import vuetify from './plugins/vuetify'
-import './assets/style.css'
+import './styles/style.css'
 
 Vue.config.productionTip = false
 
