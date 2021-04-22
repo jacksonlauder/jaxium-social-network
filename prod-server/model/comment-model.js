fd2405a45c878e5b6f29fe5287d081bc400b5f75
Jackson Lauder <jacksonelauder@gmail.com>
2021-04-22 14:55:52 -0600

    Delete prod-server directory

--- prod-server/model/comment-model.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-var _mongoose = require('mongoose');
-
-var _mongoose2 = _interopRequireDefault(_mongoose);
-
-function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
