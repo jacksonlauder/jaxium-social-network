ae036532cf020e5f73a666d31693b0832b1a5873
Jackson Lauder <jacksonelauder@gmail.com>
2021-04-23 14:20:51 -0600

    fsda

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,9 @@
 module.exports = {
   apps: [
     {
-      name: 'jaxium-social-network',
       script: 'npm',
       args: 'start',
       interpreter: 'none',
-      env: {
-        NODE_ENV: 'production',
-      },
     },
   ],
 }
