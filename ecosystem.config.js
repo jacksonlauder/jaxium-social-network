04076085e0161d29eb01b5cf318a16b5ba10f054
Jackson Lauder <jacksonelauder@gmail.com>
2021-04-23 14:04:18 -0600

    r/crossing fingers

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,8 +2,8 @@ module.exports = {
   apps: [
     {
       name: 'jaxium-social-network',
-      script: 'npx',
-      args: 'serve -s build -l 3000 -n',
+      script: 'npm',
+      args: 'start',
       interpreter: 'none',
       env: {
         NODE_ENV: 'production',
