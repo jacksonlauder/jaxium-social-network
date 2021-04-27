9a3f77b86d8177bd600399432828a567917c595b
Jackson Lauder <jacksonelauder@gmail.com>
2021-04-26 21:25:33 -0600

    asdf

--- dev-server/index.js
+++ dev-server/index.js
@@ -15,14 +15,14 @@ setEnvironment(app)
 connectToDB()
 registerRoutes(app)
 
-app.listen(3000, () => {
-  console.log('Jaxium Social Network Application listening on port 3000 in ' + process.env.NODE_ENV)
-})
-
 app.get('/', (req, res) => {
   if (process.env.NODE_ENV !== 'production') {
     return res.send('Running server in dev mode')
   } else {
     return res.sendFile('index.html', { root: __dirname + '/../dist/' })
   }
+})
+
+app.listen(3000, () => {
+  console.log('Jaxium Social Network Application listening on port 3000 in ' + process.env.NODE_ENV)
 })
