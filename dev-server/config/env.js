ae32dd5955647d0eab38dfd7f831453118582c28
Jackson Lauder <jacksonelauder@gmail.com>
2021-09-08 12:35:03 -0600

    removed body parser

--- dev-server/config/env.js
+++ dev-server/config/env.js
@@ -1,7 +1,6 @@
 import express from 'express'
 import morgan from 'morgan'
 import cors from 'cors'
-import bodyParser from 'body-parser'
 
 export function setEnvironment (app) {
   if (process.env.NODE_ENV !== 'production') {
@@ -15,7 +14,7 @@ function setDevEnv(app) {
   process.env.NODE_ENV = 'development'
   process.env.DB_URL = 'mongodb://localhost:27017/jaxium-db'
   process.env.TOKEN_SECRET = '10596874-jaxium-social-network-development-secret'
-  app.use(bodyParser.json())
+  app.use(express.json())
   app.use(morgan('dev'))
   app.use(cors())
 }
@@ -23,6 +22,6 @@ function setDevEnv(app) {
 function setProdEnv(app) {
   process.env.DB_URL = 'mongodb+srv://jacksonlauder:10596874@jaxium-db.4yzkv.mongodb.net/jaxium-db?retryWrites=true&w=majority'
   process.env.TOKEN_SECRET = '10596874-jaxium-social-network-production-secret'
-  app.use(bodyParser.json())
+  app.use(express.json())
   app.use(express.static(__dirname + '/../../dist'))
 }
